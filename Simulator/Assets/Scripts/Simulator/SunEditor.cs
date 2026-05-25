2bde7d2fa0cbf9f8f1ac4a5ebbbf9f466f7099a7
Sans505 <raulsansduarte@gmail.com>
2026-05-25 05:36:03 +0200

    Ajustes para compilar

--- Simulator/Assets/Scripts/Simulator/SunEditor.cs
+++ Simulator/Assets/Scripts/Simulator/SunEditor.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -55,3 +56,4 @@ public class SunEditor : Editor {
         sun = (Sun)target;
 	}
 }
+#endif
